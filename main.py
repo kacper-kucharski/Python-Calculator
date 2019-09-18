73ee74010ad53dfbde560f4afb2c20c7ec6993b2
Kacper Kucharski <kacpikuch1@icloud.com>
2019-09-18 20:45:10 +0200

    Initial commit added quit + help + version commands

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+import functions
+import types
+
+'''
+The entry-point of our application.
+'''
+def main():
+    # Indicates whether or not our application should quit
+    quit = False
+
+    # Implement basic functionality
+    while(not quit):
+        command = input("")
+        if command == "quit":
+            quit = True
+        elif command == "help":
+            functions.print_functions()
+        elif command == "version":
+            print("calculator version 0.1")    
+  
+if __name__== "__main__":
+    main()
