c922dd3c7a6e28ed53506f0cb3dd6cbf24f97add
Kacper Kucharski <kacpikuch1@icloud.com>
2019-10-07 21:37:18 +0200

    Implemented the Substract and Divide functions

--- arith.py
+++ arith.py
@@ -18,10 +18,11 @@ def sum(a, b):
     
     # Implement algorithm
     answer_list = []
-    # Add digits start with the most right
+    # Create a list from the given digits
     a_list = [int(i) for i in str(a)]
     b_list = [int(i) for i in str(b)]
     
+    # check which operand is bigger and use is as the first one
     if a > b:
         op1 = a_list
         op2 = b_list
@@ -29,18 +30,22 @@ def sum(a, b):
         op1 = b_list
         op2 = a_list
 
+    # reverse the lists to start from right to left
     op1.reverse()
     op2.reverse()
 
+    # Loop to the length of the largest given number + 1 (for the potential carry that has to be added in front)
     for x in range(max_length+1):
         if c > 0:
+            # Try to add all the number else try to add to the main number else just add the remaining Carry
             try:
                 r = op1[x] + op2[x] + c
             except:
                 try:
                     r = op1[x] + c
                 except:
-                    r = c     
+                    r = c    
+            # If there is no carry just add the two operands togheter else just add the main remaining operand else break out of the loop
         else:
             try:
                 r = op1[x] + op2[x]
@@ -49,20 +54,25 @@ def sum(a, b):
                     r = op1[x]
                 except:
                     break
-
+        
+        # after using the carry, set it back to 0 for next upcoming use
         c = 0
 
+        # if r is larger than 10 add 1 to carry and remove 10 from r
         if r >= 10:
             r -= 10
             c += 1
+        # then add the r to the answer_list
         answer_list.append(r)
-    
+    # Reverse the answer_list(rember we reversed it in the beginning so now we reverse it back to have the true number position)
     answer_list.reverse()
+    # Change the r variable into a empty string
     r = ''
 
+    # loop through the final answe_list and create a string from it
     for x in answer_list:
         r = r + str(x)
-    
+    # return the created string as an integer
     return int(r)
 
 '''
@@ -74,16 +84,48 @@ def sub(a, b):
     c = 0                                           # Borrow
 
     s = 1                                           # Sign is positive
+
+    answer_list = []
+    # Create a list of the given digits
+    a_list = [int(i) for i in str(a)]
+    b_list = [int(i) for i in str(b)]
+
     if b > a:
-        pass # Implement algorithm, replace pass by your own code
+        s = -1
+        op1 = b_list
+        op2 = a_list
+    else:
+        op1 = a_list
+        op2 = b_list
+
+    op1.reverse()
+    op2.reverse()
 
     length_a = arith_tools.get_number_length(a)     # Get length of number a
     length_b = arith_tools.get_number_length(b)     # Get length of number b
     max_length = max(length_a, length_b)            # Calculate maximum a if a > b, else b
     
     # Implement algorithm
+    for x in range(max_length):
+        try:
+            r = op1[x] - op2[x] - c
+        except:
+            r = op1[x] - c
 
-    return r * s                                    # Note the sign
+        c = 0
+
+        if r < 0:
+            r += 10
+            c += 1
+        answer_list.append(r)
+    
+    answer_list.reverse()
+    r = ''
+
+    for x in answer_list:
+        r = r + str(x)
+
+    return int(r) * s                                    # Note the sign
 
 '''
 Calculates the multiplication of a and b.
@@ -111,6 +153,10 @@ Only supports positive integers.
 def divide(a, b):                       
     i = 0                                     # Result
     # Implement algorithm
+    while a > b:
+        a = sub(a, b)
+        i += 1
+
     return i
 
 '''
